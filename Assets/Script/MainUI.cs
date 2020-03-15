7f28708e5cd89c3f94ccedd7f27f1832de5127ef
Mao <929764654@qq.com>
2020-03-15 18:25:14 +0800

    测试1：图像识别功能
    功能：3张固定的识别图图片识别，成功后反馈动画 ，识别图位置StreamingAssets/TargetImage
    bug：相机失真，分辨率有问题

---
+++ Assets/Script/MainUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MainUI : MonoBehaviour
+{
+    public GameObject go_PanelWarning;
+    public Text txt_Warning;
+    public GameObject go_PanelLoading;
+    public Button btn_Yes;
+
+    public BaiduARImageRecognition scr_imgRecognition;
+    public BaiduARWebCamera scr_webCamera;
+    // Use this for initialization
+    void Start()
+    {
+        btn_Yes.onClick.AddListener(() =>  Application.Quit());
+        scr_imgRecognition.OnErrorEvent.AddListener(ShowWarningHint);
+        go_PanelLoading.SetActive(true);
+        go_PanelWarning.SetActive(false);
+    }
+    private void Update()
+    {
+        //判断手机摄像机是否已启动
+        if (scr_webCamera.isLoad)
+        {
+            go_PanelLoading.SetActive(false); 
+        }
+    }
+
+    /// <summary>
+    /// 事件：id或密钥错误时调用
+    /// </summary>
+    private void ShowWarningHint(string num,string msg)
+    {
+        go_PanelWarning.SetActive(true);
+        txt_Warning.text = "num:" + num + ",msg:" + msg;
+    }
+}
---
+++ Assets/Script/ScanImage.cs
@@ -0,0 +1,41 @@
+﻿using BaiduARUnitySDK;
+using UnityEngine;
+/// <summary>
+/// 扫描图处理类
+/// </summary>
+public class ScanImage : MonoBehaviour
+{
+    public GameObject go_Prefab;                 //扫码后所显示的预设体
+    private GameObject m_MyProfab = null;        //判定是否已生成预设
+    private Transform m_ParentContainer;         //容器 用于存放预设体控制其隐藏
+    private BaiduARImageRecognitionResult result; 
+    void Start()
+    {
+        Screen.SetResolution(1080, 1920,true);
+        result = GetComponent<BaiduARImageRecognitionResult>();
+        result.OnRespond.AddListener(ResultEvent);
+        m_ParentContainer = GameObject.Find("Parent").transform; 
+    }
+    /// <summary>
+    /// 识别到识别图之后触发
+    /// </summary>
+    private void ResultEvent()
+    {
+        Debug.Log("已识别到识别图");
+        for (int i = 0; i < m_ParentContainer.childCount; i++)
+        {
+            m_ParentContainer.GetChild(i).gameObject.SetActive(false);
+            Debug.Log("隐藏容器所有物体");
+        }
+        if (m_MyProfab == null)
+        {
+            m_MyProfab = Instantiate(go_Prefab);
+            m_MyProfab.transform.SetParent(m_ParentContainer);
+            Debug.Log("实例化生成特效，放置在容器下");
+        }
+        else
+        {
+            m_MyProfab.gameObject.SetActive(true);
+        }
+    }
+}
